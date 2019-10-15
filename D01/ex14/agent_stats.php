932e29f19d9b9d82ec35acd4704ca5ff1895b33a
samuelsit <samsitruk@gmail.com>
2019-10-15 21:54:04 +0200

    ex14

--- D01/ex14/agent_stats.php
+++ D01/ex14/agent_stats.php
@@ -2,26 +2,59 @@
 <?php
 if ($argc == 2) {
     $total = 0;
-    $nb = 0;
-    if ($argv[1] == 'moyenne') {
-        while ($line = fgets(STDIN)) {
-            $tab = explode(";", $line);
-            if ($tab[1] != "" && is_numeric($tab[1]) && $tab[2] != "moulinette") {
-                $note = $tab[1];
-                $total += $note;
-                $nb++;
+    $occurence = 0;
+    $line = array();
+    $user = array();
+    fgets(STDIN);
+    while ($lstdin = fgets(STDIN)) {
+        $stat = explode(";", $lstdin);
+        if (count($stat) == 4) {
+            $line[] = $stat;
+            if (!isset($user[$stat[0]])) {
+                $user[$stat[0]]['name'] = $stat[0];
+                $user[$stat[0]]['occurence'] = 0;
+                $user[$stat[0]]['total'] = 0;
+                $user[$stat[0]]['mouli'] = 0;
             }
         }
-        $moyenne = $total / $nb;
+    }
+    ksort($user);
+    if ($argv[1] == 'moyenne') {
+        foreach ($line as $key) {
+            if ($key[1] != '' && $key[2] != 'moulinette') {
+                $total += $key[1];
+                $occurence++;
+            }
+        }
+        $moyenne = $total / $occurence;
         echo $moyenne."\n";
     }
     else if ($argv[1] == 'moyenne_user') {
-        $csv = array();
-        while ($line = fgets(STDIN)) {
-            $csv[] = $line;
+        foreach ($line as $key) {
+            if ($key[1] != '' && $key[2] != 'moulinette') {
+                $user[$key[0]]['occurence'] += 1;
+                $user[$key[0]]['total'] += $key[1];
+            }
         }
-        sort($csv);
-        
+        foreach ($user as $stud) {
+            $moyenne = $stud['total'] / $stud['occurence'];
+            echo $stud['name'].":".$moyenne."\n";
+        }
     }
-}//31 adam
+    else if ($argv[1] == 'ecart_moulinette') {
+        foreach ($line as $key) {
+            if ($key[1] != '' && $key[2] != 'moulinette') {
+                $user[$key[0]]['occurence'] += 1;
+                $user[$key[0]]['total'] += $key[1];
+            }
+            else if ($key[2] == 'moulinette') {
+                $user[$key[0]]['mouli'] = $key[1];
+            }
+        }
+        foreach ($user as $stud) {
+            $moyenne = $stud['total'] / $stud['occurence'] - $stud['mouli'];
+            echo $stud['name'].":".$moyenne."\n";
+        }
+    }
+}
 ?>
