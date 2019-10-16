5f5860613c19217534eb382af23452b498e54579
Samuel SITRUK <ssitruk@e1r10p21.42.fr>
2019-10-16 18:41:02 +0200

    d1

--- D01/ex02/oddeven.php
+++ D01/ex02/oddeven.php
@@ -1,24 +1,21 @@
 #!/usr/bin/php
 <?php
 
-function isOdd($nb)
-{
-	if ($nb % 2 == 0)
-		echo "Le chiffre $nb est Pair\n";
-	else
-		echo "Le chiffre $nb est Impair\n";
-}
-
 while (1)
 {
 	echo "Entrez un nombre: ";
 	$nb = trim(fgets(STDIN));
+	$isOdd = substr($nb, -1, 1);
 	if (feof(STDIN) == TRUE)
 		exit;
 	if (!is_numeric($nb))
 		print("'$nb' n'est pas un chiffre\n");
-	else
-		isOdd($nb);
+	else {
+		if ($isOdd % 2 == 0)
+			echo "Le chiffre $nb est Pair\n";
+		else
+			echo "Le chiffre $nb est Impair\n";
+	}
 }
 
 ?>
