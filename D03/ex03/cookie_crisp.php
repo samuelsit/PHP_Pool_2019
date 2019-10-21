2602743287e34121abd7287f2808de9c5c30833c
Samuel SITRUK <ssitruk@e1r10p18.42.fr>
2019-10-21 12:46:43 +0200

    d3-d4

--- D03/ex03/cookie_crisp.php
+++ D03/ex03/cookie_crisp.php
@@ -1,11 +1,11 @@
 <?php
 
-if (isset($_GET['action']) && isset($_GET['name']) && isset($_GET['value']) && $_GET['action'] == 'set')
-	setcookie($_GET['name'], $_GET['value'], time() + 365*24*60*60);
+if ($_GET && $_GET['action'] == 'set')
+	setcookie($_GET['name'], $_GET['value'], time() + 24*60*60);
 
-elseif (isset($_GET['action']) && isset($_GET['name']) && $_GET['action'] == 'get' && isset($_COOKIE[$_GET['name']]))
+elseif ($_GET && $_GET['action'] == 'get' && $_COOKIE[$_GET['name']])
 	echo $_COOKIE[$_GET['name']] . "\n";
 
-elseif (isset($_GET['action']) && isset($_GET['name']) && $_GET['action'] == 'del')
+elseif ($_GET['action'] && $_GET['name'] && $_GET['action'] == 'del')
 	setcookie($_GET['name']);
 ?>
